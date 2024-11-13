84ea5ad40cc0a14e3ef4eb071d6ab7e6a32cf5d6
ChrispinSantacruz <christian.santacruzl@campusucc.edu.co>
2024-11-13 17:14:22 -0500

    intento2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
-import 'package:apptesis/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(), 
+      home: HomeScreen(),
     );
   }
 }
